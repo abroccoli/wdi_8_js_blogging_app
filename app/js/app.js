24dde420146f28fe9fabc9c8f02d5e8bb16c868f
fishermanswharff <fishermanswharff@github.com>
2015-03-04 19:46:04 -0500

    initial commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,4 @@
+/*global $:false*/
 'use strict';
 
 var trace = function(){
@@ -6,20 +7,59 @@ var trace = function(){
   }
 };
 
-var App = App || {};
-
-App.square = function(x){
-  return x * x
+var App = App || {
+  url: 'http://localhost:3000'
 };
 
-App.cube = function(x){
-  return x * x * x
+App.getPosts = function(){
+  $.ajax({
+    url: App.url + '/posts',
+    type: 'GET',
+  }).done(function(data){
+    App.indexPosts(data);
+  }).fail(function(jqXHR, textStatus, errorThrown){
+    trace(jqXHR, textStatus, errorThrown);
+  });
 };
 
-App.greet = function(string){
-  return string = typeof string !== 'undefined' ? 'Hello ' + string : "Hello World";
+App.indexPosts = function(posts){
+  posts.forEach(App.renderPost);
+};
+
+App.renderPost = function(currentVal,index,array){
+  $('div.row').append('<article class="post">' + '<h1 class="post-title">' + currentVal.title + '</h1>' + '<p class="post-body">' + currentVal.body + '</p>' + '<small class="post-author">' + currentVal.author + '</small>' + '<p>Comments: ' + currentVal.comments.length + '</p>' + '</article>');
+};
+
+App.submitPost = function(event){
+  if(event.preventDefault) event.preventDefault();
+  $.ajax({
+    url: App.url + '/posts',
+    type: 'POST',
+    data: { post: {
+        title: $('input#post-title').val(),
+        body: $('textarea#post-body').val(),
+        author: $('input#post-author').val()
+      }
+    },
+  }).done(function(data){
+    window.location.href = '/';
+  }).fail(function(jqXHR,textStatus,errorThrown){
+    trace('error, error, error');
+  });
 };
 
 $(document).ready(function(){
+  App.getPosts();
+  $('form.new-post-form').on('submit',function(event){
+    App.submitPost(event);
+  });
   trace('hello world');
 });
+
+
+
+
+
+
+
+
